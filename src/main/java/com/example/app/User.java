1297401227e55867f239ede1bef83a40b656f0e3
Ryutaro_Kosuge <bp19116@shibaura-it.ac.jp>
2021-12-18 14:10:09 +0900

    user名前

--- src/main/java/com/example/app/User.java
+++ src/main/java/com/example/app/User.java
@@ -1,5 +1,36 @@
 package com.example.app;
 
 public class User {
-
+	String userID = "";
+	String webSocketID = "";
+	int status;
+	String gameID = "";
+	
+	String getID() {
+		return userID;
+	}
+	
+	void setWebSocketID(String ID){
+		
+	}
+	
+	String getWebSocketID() {
+		return webSocketID;
+	}
+	
+	void setGameID(String ID) {
+		
+	}
+	
+	String getGameID() {
+		return gameID;
+	}
+	
+	void setStatus(int number) {
+		
+	}
+	
+	int getStatus() {
+		return status;
+	}
 }
