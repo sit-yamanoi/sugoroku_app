4e9ee8c38839088c82d6fbb2ee69bed7807318d0
Ryutaro_Kosuge <bp19116@shibaura-it.ac.jp>
2021-12-19 12:05:20 +0900

    mapだいたいsquareちょっと

--- src/main/java/com/example/app/Map.java
+++ src/main/java/com/example/app/Map.java
@@ -1,5 +1,75 @@
 package com.example.app;
 
 public class Map {
-
+	Square start = null;
+	Square goal = null;
+	int[][]  mapStr; //マップ構造
+	int[][] mapLen; //長い方のルートの長さ
+	
+	Square getstart() {
+		return start;
+	}
+	
+	Square getgoal() {
+		return goal;
+	}
+	
+	void genSquare(int mapnum) { //mapnum:マップ番号
+		int m = 0; //配列カウント
+		int i = 0; //ループ用
+		int j = 0; //メインルートマスカウント
+		int r = 0; //分岐カウント
+		
+		int fs, fg; //分岐記憶
+		Square current = null; //作成したマス
+		Square prev = null; //前のマス
+		Square forkStart = null; //分岐開始点
+		Square forkGoal = null; //分岐終了点
+		prev = forkStart;
+		
+		do {
+			fs = mapStr[mapnum][m];
+			m++;
+			fg = mapStr[mapnum][m];
+			m++;
+			j = mapStr[mapnum][m];
+			
+			
+			if(fs == 0) {
+				start = new Square(mapStr[mapnum][m]);
+				prev = start;
+			}else {
+				forkStart = new Square(mapStr[mapnum][m]);
+			}
+			
+			
+			for(j = 1; j < mapLen[mapnum][r]; m++, j++) {
+				current = new Square(mapStr[mapnum][m]);
+				current.prev0 = prev;
+				prev.next0 = current;
+				prev = current;
+				}
+			
+			if(fs == 0) {
+				goal = current;
+			}else {
+				forkStart = current;
+			}
+			
+			if(fs != 0) {
+				forkGoal = current;
+				current = start;
+				for(i = 0; i < fs; i++) {
+					current = current.next0;
+				}
+				current.next1 = forkStart;
+				forkStart.prev0 = current;
+				for(i = 0; i < fg; i++) {
+					current = current.next0;
+				}
+				current.prev1 = forkGoal;
+				forkGoal.next0 = current;
+			}
+		}while(m < mapStr[mapnum].length);
+	}
 }
--- src/main/java/com/example/app/Square.java
+++ src/main/java/com/example/app/Square.java
@@ -5,7 +5,12 @@ public class Square {
 	Square prev1 = null;
 	Square next0 = null;
 	Square next1 = null;
-	int effectID;
+	int effectID = 0; //0:何もなし 1:getItem 2:goFoward 3:backFoward 4:crewRestart 5:crewTakeItem 6:crewGiveItem
+	
+	
+	Square(int id) {
+		effectID = id;
+	}
 	
 	int getEfectID() {
 		return effectID;
