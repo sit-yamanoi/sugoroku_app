ac4b53a0496b25fbe0322840fe8913e02e624857
sukes <sukes@192.168.11.9>
2021-12-30 17:01:56 +0900

    maplen

--- src/main/java/com/example/app/GameMap.java
+++ src/main/java/com/example/app/GameMap.java
@@ -3,8 +3,8 @@ package com.example.app;
 public class GameMap {
 	Square start = null;
 	Square goal = null;
-	int[][] mapStr; //マップ構造 [分岐スタート地点][合流地点][コマの効果][...]の順 0,1列目は全て0
-	int[][] mapLen; //ルートの長さ [メインの長さ][分岐の長さ][分岐の長さ]の順
+	int[] mapStr; //マップ構造 [分岐スタート地点][合流地点][コマの効果][...]の順 0,1列目は全て0
+	int[] mapLen; //ルートの長さ [メインの長さ][分岐の長さ][分岐の長さ]の順
 	
 	Square getstart() {
 		return start;
@@ -27,23 +27,23 @@ public class GameMap {
 		Square forkGoal = null; //分岐終了点
 		
 		do {
-			fs = mapStr[mapnum][m];
+			fs = mapStr[m];
 			m++;
-			fg = mapStr[mapnum][m];
+			fg = mapStr[m];
 			m++;
 			
 			
 			if(fs == 0) {
-				start = new Square(mapStr[mapnum][m]);
+				start = new Square(mapStr[m]);
 				prev = start;
 			}else {
-				forkStart = new Square(mapStr[mapnum][m]);
+				forkStart = new Square(mapStr[m]);
 				prev = forkStart;
 			}
 			
 			
-			for(j = 1; j < mapLen[mapnum][r]; m++, j++) {
-				current = new Square(mapStr[mapnum][m]);
+			for(j = 1; j < mapLen[r]; m++, j++) {
+				current = new Square(mapStr[m]);
 				current.prev0 = prev;
 				prev.next0 = current;
 				prev = current;
@@ -71,6 +71,6 @@ public class GameMap {
 			
 			r++;
 			
-		}while(m < mapStr[mapnum].length);
+		}while(m < mapStr.length);
 	}
 }
