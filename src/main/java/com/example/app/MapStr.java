d603ddb83d0b200b213f6fa373d73d6dc816d4e7
sukes <sukes@192.168.2.104>
2022-01-10 22:08:03 +0900

    test関連

--- src/main/java/com/example/app/MapStr.java
+++ src/main/java/com/example/app/MapStr.java
@@ -4,7 +4,12 @@ public class MapStr {
 	//マップ構造 [分岐スタート地点][合流地点][コマの効果][...]の順 0,1列目は全て0
 	//ルートの長さ [メインの長さ][分岐の長さ][分岐の長さ]の順
 	//0:何もなし 1:getItem 2:goFoward 3:backFoward 4:crewRestart 5:crewTakeItem 6:crewGiveItem 7:振り出し
-	static int[] map =	{0, 0,	
-			
-	};
+	static int[] str =	{0, 0,	
+						 0, 0, 1, 1, 2, 2, 0, 0, 0, 1,
+						 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+						 2, 2, 2, 3, 0, 0, 0, 0, 0, 0,
+						 15,20,
+						 4, 1, 4, 2};
+	
+	static int[] len = {30, 4};
 }
